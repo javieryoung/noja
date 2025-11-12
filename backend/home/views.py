b601024e6d1ee6edfe98a81024e242d0dafcdb1b
pablo-riveiro-uy <priveiro@gmail.com>
2025-11-12 10:27:59 -0300

    adding countries select with constants file

--- backend/home/views.py
+++ backend/home/views.py
@@ -1,13 +1,16 @@
 from django.views import View
 from django.shortcuts import render
 from .forms import CustomUserCreationForm
-
+from .constants import COUNTRIES_KEYS
 
 
 class Homepage(View):
     def get(self, request):
         form = CustomUserCreationForm()
-        return render(request, 'home-content.html', context={'form': form})
+        return render(request, 'home-content.html', context={
+            'form': form,
+            'countries': COUNTRIES_KEYS
+        })
 
     def post(self, request):
         form = CustomUserCreationForm(request.POST)
@@ -15,8 +18,12 @@ class Homepage(View):
             form.save()
             return render(request, 'home-content.html', context={
                 'form': CustomUserCreationForm(),  # formulario limpio
-                'success_message': 'Formulario guardado correctamente'
+                'success_message': 'Formulario guardado correctamente',
+                'countries': COUNTRIES_KEYS
             })
         else:
-            return render(request, 'home-content.html', context={'form': form})
+            return render(request, 'home-content.html', context={
+                'form': form,
+                'countries': COUNTRIES_KEYS
+            })
 
