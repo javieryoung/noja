ceb3fe6c458495ea80b324237435c0b48d4726a7
pablo-riveiro-uy <priveiro@gmail.com>
2025-11-17 11:57:12 -0300

    adding suggestion model

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django import forms
 from tinymce.widgets import TinyMCE
-from .models import New
+from .models import New, Suggestion
 
 class NewAdminForm(forms.ModelForm):
     content = forms.CharField(widget=TinyMCE(mce_attrs={
@@ -17,6 +17,13 @@ class NewAdminForm(forms.ModelForm):
         model = New
         fields = '__all__'
 
+class SuggestionInline(admin.TabularInline):  # o admin.StackedInline si preferís más detalle
+    model = Suggestion
+    extra = 0  # No mostrar filas vacías por defecto
+    fields = ('title', 'direction', 'description', 'symbol', 'date')
+    readonly_fields = ()  # Podés agregar campos no editables si querés
+    show_change_link = True 
+
 @admin.register(New)
 class NewAdmin(admin.ModelAdmin):
     form = NewAdminForm
@@ -24,3 +31,4 @@ class NewAdmin(admin.ModelAdmin):
     fields = ('title', 'subtitle', 'content', 'image', 'date')
     search_fields = ('title', 'subtitle')
     list_filter = ('date',)
+    inlines = [SuggestionInline]
