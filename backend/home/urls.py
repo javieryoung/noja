c63ab3c5368228d5b171e88d0c98a10e166b0ba7
pablo-riveiro-uy <priveiro@gmail.com>
2025-11-18 11:32:05 -0300

    making home the project root

--- backend/home/urls.py
+++ backend/home/urls.py
@@ -3,7 +3,7 @@ from .views import Homepage, new_detail, news_list
 
 
 urlpatterns = [
-    path('homepage/', Homepage.as_view(), name='Homepage'),
+    path('', Homepage.as_view(), name='Homepage'),
     path('new/<int:pk>/', new_detail, name='new_detail'),
     path('news/', news_list, name='news_list'),
 
